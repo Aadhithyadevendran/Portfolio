8e48eb7428ed9bf01110e45207e08be971978d38
Aadhithya D <119392778+Aadhithyadevendran@users.noreply.github.com>
2025-08-05 21:22:46 +0530

    Logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -22,12 +22,14 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-screen h-[80px] z-50 bg-white dark:bg-bodyColor text-black dark:text-white flex justify-between items-center font-titleFont border-b border-gray-300 dark:border-gray-600 px-4 sm:px-8">
-      <div className="ml-0">
+    <div className="fixed top-0 left-0 w-screen h-[80px] z-50 bg-white dark:bg-bodyColor text-black dark:text-white flex justify-between items-center font-titleFont border-b border-gray-300 dark:border-gray-600 px-4 sm:px-4">
+     
+
+<div className="ml-0 !ml-0 p-0">
         <img
           src={logo}
           alt="logo"
-          className="w-11 sm:w-14 md:w-14 h-auto object-contain"
+          className="w-10 sm:w-10 md:w-14 h-auto object-contain"
         />
       </div>
      
